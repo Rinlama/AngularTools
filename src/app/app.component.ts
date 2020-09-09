cf8278bde7ad2bf9179571092c14bba72b9f24ae
rinlama <rinjinlama83@gmail.com>
2020-09-09 13:10:48 -0400

    add excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import * as XLSX from 'xlsx';
 })
 export class AppComponent {
   title = 'toolsets';
+  html = '';
 
   readExcel(evt: any) {
     const target: DataTransfer = evt.target as DataTransfer;
@@ -24,12 +26,33 @@ export class AppComponent {
       const wsname: string = wb.SheetNames[0];
 
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-      console.log(ws);
       //  console.log(ws);
-      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      // console.log(data);
+      const data = XLSX.utils.sheet_to_json(ws);
+      console.log(data);
     };
 
     reader.readAsBinaryString(target.files[0]);
+  }
+
+  createExcel() {
+    const data = [{ name: 'test', subject: 'he' }];
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { data: worksheet },
+      SheetNames: ['test'],
+    };
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      bookSST: false,
+      type: 'array',
+    });
+    console.log(excelBuffer);
+    const blobData: Blob = new Blob([excelBuffer], {
+      type:
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel',
+    });
+
+    console.log(blobData);
+    // FileSaver.saveAs(blobData, 'trst_export_.xlsx');
   }
 }
