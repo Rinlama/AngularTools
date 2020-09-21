0e029e41e46cf683ad001f50c800977f8503155d
rinlama <rinjinlama83@gmail.com>
2020-09-20 20:21:58 -0400

    mat forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  FormArray,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'toolsets';
+export class AppComponent implements OnInit {
+  customerForm: FormGroup;
+  dynamicForms: FormArray;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+        ],
+      ],
+      age: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.min(18),
+          Validators.max(65),
+        ],
+      ],
+      cellPhone: this.newPhone(),
+      homePhone: this.newPhone(),
+      dynamicPhone: this.fb.array([this.addPhoneGroup()]),
+    });
+  }
+
+  getFirstName() {
+    return this.customerForm.get('firstName');
+  }
+  getPassword() {
+    return this.customerForm.get('password');
+  }
+  getAge() {
+    return this.customerForm.get('age');
+  }
+
+  addPhoneGroup() {
+    return this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+  }
+
+  getPhoneArray(): FormArray {
+    return this.customerForm.get('dynamicPhone') as FormArray;
+  }
+
+  addPhone() {
+    const phone = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+    this.getPhoneArray().push(phone);
+  }
+
+  deletePhone(i) {
+    this.getPhoneArray().removeAt(i);
+  }
+
+  newPhone() {
+    return this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+  }
+  submitHandler() {
+    console.log(this.customerForm.value);
+  }
 }
--- src/app/mat/mat.module.ts
+++ src/app/mat/mat.module.ts
@@ -1,8 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,14 +12,14 @@ import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatModule } from './mat/mat.module';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    MatModule,
+  declarations: [],
+  imports: [],
+  exports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatBadgeModule,
@@ -36,9 +33,8 @@ import { MatModule } from './mat/mat.module';
     ReactiveFormsModule,
     MatGridListModule,
     MatSlideToggleModule,
+    MatDividerModule,
+    MatListModule,
   ],
-  providers: [],
-
-  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class MatModule {}
