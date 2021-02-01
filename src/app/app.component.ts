9bb6ee091436c3587f59019dde384d641557f292
rinlama <rinjinlama83@gmail.com>
2021-01-31 23:20:46 -0500

    git combinedLatest and withLatestFrom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, combineLatest, BehaviorSubject } from 'rxjs';
+import { withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'toolsets';
+export class AppComponent implements OnInit {
+  framework: Array<any> = ['React', 'Angular', 'Spring Boot', 'Laravel'];
+  color: Array<any> = ['red', 'yellow', 'blue'];
+
+  fwSubject = new BehaviorSubject<any>('React');
+  colorSubject = new BehaviorSubject<any>('red');
+
+  ngOnInit(): void {
+    combineLatest([
+      this.fwSubject.asObservable(),
+      this.colorSubject.asObservable(),
+    ]).subscribe((d) => {
+      console.log(d);
+    });
+
+    this.fwSubject.pipe(withLatestFrom(this.colorSubject)).subscribe((d) => {
+      console.log(d);
+    });
+  }
+  onFWChange($event) {
+    const value = $event.target.value;
+    this.fwSubject.next(value);
+  }
+  onColorChange($event) {
+    const value = $event.target.value;
+    this.colorSubject.next(value);
+  }
 }
