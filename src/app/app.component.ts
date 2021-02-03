4db7dce0fe087ba55568c663fa4586785c7e348e
rinlama <rinjinlama83@gmail.com>
2021-02-02 23:14:53 -0500

    merge map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { map, mergeAll, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'toolsets';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const food = ['steak', 'sandwich'];
+
+    from(food)
+      .pipe(
+        mergeMap((d) => {
+          return this.getMessage(d);
+        })
+      )
+      .subscribe((d) => {
+        console.log(d);
+      });
+
+    from(food)
+      .pipe(
+        map((d) => {
+          return this.getMessage(d);
+        }),
+        mergeAll()
+      )
+      .subscribe((d) => {
+        console.log(d);
+      });
+  }
+
+  getMessage(food) {
+    return of(food + ' love it');
+  }
 }
