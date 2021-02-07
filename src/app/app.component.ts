46cd39ab52049fe92cb962459e229850cb376b0e
rinlama <rinjinlama83@gmail.com>
2021-02-07 15:09:53 -0500

    map switchall switchmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import {
+  concatAll,
+  concatMap,
+  delay,
+  map,
+  mergeAll,
+  switchAll,
+  switchMap,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toolsets';
+
+  ngOnInit() {
+    const food = ['burger', 'sandwich', 'pizza'];
+
+    const food$ = from(food);
+
+    food$
+      .pipe(
+        map((data) => {
+          return this.getData(data);
+        })
+      )
+      .subscribe((d) => {
+        console.log(d);
+      });
+  }
+
+  getData(message): Observable<string> {
+    return of(message + ' I love it').pipe(delay(1000));
+  }
 }
