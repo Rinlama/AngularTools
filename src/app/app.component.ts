c9970f5c48acd441cc83337dfde9d287664d3157
rinlama <rinjinlama83@gmail.com>
2021-02-07 15:48:23 -0500

    concat collection rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { concatAll, concatMap, delay, map, mergeAll } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toolsets';
+
+  ngOnInit(): void {
+    const food = ['pizza', 'sandwich', 'burger'];
+
+    const $food = from(food);
+
+    $food.pipe(concatMap((d) => this.getData(d))).subscribe((d) => {
+      console.log(d);
+    });
+  }
+
+  getData(food) {
+    return of(food + ' I love it').pipe(delay(1000));
+  }
 }
