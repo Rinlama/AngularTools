112e7ab848a569fcde6aa24467b70a251daa66fd
rinlama <rinjinlama83@gmail.com>
2021-02-20 22:45:39 -0500

    add interval & timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toolsets';
+  data: number;
+
+  ngOnInit(): void {
+    // const obs$ = interval(2000);
+
+    const ons$ = timer(5000, 1000);
+    ons$.subscribe((d) => {
+      console.log(d);
+      this.data = d;
+    });
+  }
 }
