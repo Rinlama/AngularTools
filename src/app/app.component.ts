277af7284b03a223a5c9d16fecf7220ecd089711
rinlama <rinjinlama83@gmail.com>
2021-02-20 21:49:52 -0500

    shareReplay init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toolsets';
+
+  constructor(private httpclient: HttpClient) {}
+
+  ngOnInit() {
+    const ob$ = this.httpclient
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .pipe(shareReplay());
+
+    ob$.subscribe((d) => {
+      console.log(d);
+    });
+
+    ob$.subscribe((d) => {
+      console.log(d);
+    });
+  }
 }
