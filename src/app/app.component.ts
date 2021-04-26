d9fb583d213e2416214955a9f0510e8fd21dff7d
rinlama <rinjinlama83@gmail.com>
2021-04-25 20:03:03 -0400

    checkbox add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'toolsets';
+  parentSelector: boolean = false;
+  food = [
+    { id: 1, select: false, name: 'dumpling' },
+    { id: 2, select: true, name: 'burger' },
+    { id: 3, select: true, name: 'sandwich' },
+  ];
+
+  onChangeFood($event) {
+    const id = $event.target.value;
+    const isChecked = $event.target.checked;
+
+    this.food = this.food.map((d) => {
+      if (d.id == id) {
+        d.select = isChecked;
+        this.parentSelector = false;
+        return d;
+      }
+      if (id == -1) {
+        d.select = this.parentSelector;
+        return d;
+      }
+      return d;
+    });
+    console.log(this.food);
+  }
 }
